198e1eac916ea9276899c1fe7097b498b83cc6f8
RafaelGermano05 <rgermanovasconcelos@gmail.com>
2025-04-28 17:52:59 -0300

    acesso iara

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
               "rafael", "daniel", "aline", "dougllas", "sarah", 
               "linaldo", "kesse", "rozelia", "aldenir", "pedro", "gabriel", 
               "thiago", "thiago.correa", "guilherme", "brito", "hygor", "allex", "anita",
-              "lenise", "raira", "berg", "andre", "yara"
+              "lenise", "raira", "berg", "andre", "iara"
           ];
           
           const validPassword = "123456";
